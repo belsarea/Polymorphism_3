68b1c8f9ced0d6306d889e9d6532873dbcc00c46
belsarea <anup.belsare@cognizant.com>
2017-05-11 07:40:30 +0000

    commit changes

--- server.js
+++ server.js
@@ -8,4 +8,12 @@ mongoose.connect('mongodb://localhost:27017/mittens');
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
-
+app.use(express.static('public')); 
+var Meow = mongoose.model('Meow', {
+ text: String
+  });
+app.get('/meows',function(req,res,next){
+Meow.find({},function(err,meows){
+return res.json(meows);
+});
+});
