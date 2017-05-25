e94ef2a97769ab4eb9ee4bc9bfac60778240720f
belsarea <anup.belsare@cognizant.com>
2017-05-25 07:04:47 +0000

    session 5 commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose=require('mongoose');
 var app = express();
+mongoose.Promise = global.Promise;
 app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost:27017/mittens');
 
@@ -27,19 +28,11 @@ app.post('/meows', function(req, res, next){
 });
 var User=mongoose.model('User', { 
 username: String, password: String  });
-app.post('/users', function(req, res, next){		
-			var newUser= new User({
-				username: req.body.username,
-				password: req.body.password
-			});
-			newUser.save( function(err){
-			return res.send();
-	});		
-}) ;
 var bcrypt = require('bcryptjs');
 app.post('/users', function(req, res, next){	
 	bcrypt.genSalt(10, function(err,salt){
 		bcrypt.hash(req.body.password,salt,function(err,hash){
+			console.log(hash);
 			var newUser= new User({
 				username: req.body.username,
 				password: hash
